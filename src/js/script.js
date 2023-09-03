16f9620ca3d42807b9fdd75a920438f0450c60fd
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-02 23:36:38 -0400

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// Add your solution here
+<script>
+    {/* // Add your JavaScript here
+
+    // Example: Close the message box when the 'X' button is clicked
+    document.getElementById('close-button').addEventListener('click', function() {
+      document.getElementById('message-box').style.display = 'none';
+    }); */}
+  </script>
