39c0f8702fb96865d5589bb2c3d653f241600311
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-03 13:48:38 -0400

    Add aria live to error messages

--- src/js/script.js
+++ src/js/script.js
@@ -12,36 +12,33 @@ document.addEventListener('DOMContentLoaded', () => {
       e.preventDefault();
   
       // Hide all previous error messages
-    //   const errorMessages = document.querySelectorAll('.error-message');
-    //   errorMessages.forEach(msg => msg.style.display = "none");
+      const errorMessages = document.querySelectorAll('.error-message');
+      errorMessages.forEach(msg => msg.style.display = "none");
   
       // Validate Full Name
       const fullName = document.getElementById('full-name').value.trim();
       if (!fullName) {
-        document.getElementById('full-name-error').style.display = 'block';
-      } else {
-        document.getElementById('full-name-error').style.display = 'none';
-      }
+          document.getElementById('full-name-error').style.display = 'block';
+      } 
+
   
       // Validate Email
       const email = document.getElementById('email').value.trim();
-    if (!email) {
-      document.getElementById('missing-email').style.display = 'block';
-    } else if (!email.includes('@')) {
-      document.getElementById('missing-email').style.display = 'none';
-      document.getElementById('missing-@').style.display = 'block';
-    } else {
-        document.getElementById('missing-email').style.display = 'none';
-        document.getElementById('missing-@').style.display = 'none';
-    }
+      if (!email) {
+          document.getElementById('missing-email').style.display = 'block';
+      } 
+      
+      if (email && !email.includes('@')) {
+          document.getElementById('missing-email').style.display = 'none';
+          document.getElementById('missing-@').style.display = 'block';
+      } 
   
       // Validate Message
       const message = document.getElementById('message').value.trim();
       if (!message) {
-        document.getElementById('message-error').style.display = 'block';
-      } else {
-        document.getElementById('message-error').style.display = 'none';
-      }
+          document.getElementById('message-error').style.display = 'block';
+      } 
+     
     });
   });
   
