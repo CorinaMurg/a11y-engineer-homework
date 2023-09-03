6b1be27981c5d47165aa7a0bdea798ea73617c7b
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-03 09:02:51 -0400

    Clean CSS for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,5 @@
-// Add your solution here
-<script>
-    {/* // Add your JavaScript here
 
-    // Example: Close the message box when the 'X' button is clicked
-    document.getElementById('close-button').addEventListener('click', function() {
-      document.getElementById('message-box').style.display = 'none';
-    }); */}
-  </script>
+document.getElementById('great-day--button').addEventListener('click', function() {
+    document.getElementById('great-day').style.display = 'none';
+}); 
+
