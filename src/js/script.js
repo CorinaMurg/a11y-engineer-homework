641a97fc7ef7298da3172c2145d97f74ce824183
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-03 18:20:48 -0400

    Add error border class with red border

--- src/js/script.js
+++ src/js/script.js
@@ -14,31 +14,41 @@ document.addEventListener('DOMContentLoaded', () => {
     
     let errors = 0;
     let errorMessages = [];
-    let firstErrorField = null;
+    // USED WHEN TRIED TO BRING FOCUS TO FIRST ERROR FIELD
+    // let firstErrorField = null;
 
-    // Hide all previous error messages
+    // REMOVES ERROR-BORDER CLASS FROM ALL FIELDS
+    document.querySelectorAll('.error-border').forEach(el => el.classList.remove('error-border'));
+
+
+    // HIDES ALL PREVIOUS ERROR MESSAGES
     const errorMessagesEl = document.querySelectorAll('.error-message');
     errorMessagesEl.forEach(msg => msg.style.display = "none");
 
-    // Validate Full Name
-    const fullName = document.getElementById('full-name').value.trim();
+    // VALIDATE FULL NAME
+    const fullNameField = document.getElementById('full-name');
+    const fullName = fullNameField.value.trim();
     if (!fullName) {
         document.getElementById('full-name-error').style.display = 'block';
+        fullNameField.classList.add('error-border');  // Add error-border class
         errors++;
         errorMessages.push('Full name is required.');
+    
         // failed attemp to bring focus to first error field; brings focus to the field
         // and reads the field info before reading the error messages; tried with both "polite" and "assertive
         // firstErrorField = firstErrorField || 'full-name';
     } 
 
-    // Validate Email
-    const email = document.getElementById('email').value.trim();
+    // VALIDATE EMAIL
+    const emailField = document.getElementById('email');
+    const email = emailField.value.trim();
     if (!email) {
         document.getElementById('missing-email').style.display = 'block';
+        emailField.classList.add('error-border');  
         errors++;
         errorMessages.push('Email is required.');
-        // firstErrorField = firstErrorField || 'email';
-    } 
+    }
+    // REPLACED BY THE BUILD-IN HTML5 EMAIL VALIDATION
     // else if (!email.includes('@')) {
     //     document.getElementById('missing-@').style.display = 'block';
     //     errors++;
@@ -46,12 +56,15 @@ document.addEventListener('DOMContentLoaded', () => {
     //     // firstErrorField = firstErrorField || 'email';
     // }
 
-    // Validate Message
-    const message = document.getElementById('message').value.trim();
+    // VALIDATE MESSAGE
+    const messageField = document.getElementById('message');
+    const message = messageField.value.trim();
     if (!message) {
         document.getElementById('message-error').style.display = 'block';
+        messageField.classList.add('error-border');  // Add error-border class
         errors++;
         errorMessages.push('Message is required.');
+  
         // firstErrorField = firstErrorField || 'message';
     }
 
