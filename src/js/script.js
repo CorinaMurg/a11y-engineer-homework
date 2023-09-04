3d94d7ce08ae0a38290948f06534ee897dfb1548
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-03 21:10:29 -0400

    Add function to handle error remove for all inputs

--- src/js/script.js
+++ src/js/script.js
@@ -8,71 +8,53 @@ document.getElementById('great-day--button').addEventListener('click', function(
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
   const formError = document.getElementById('form-error');
+
+  const fields = [
+    { id: 'full-name', errorMsgId: 'full-name-error', errMsg: 'Full name is required.' },
+    { id: 'email', errorMsgId: 'missing-email', errMsg: 'Email is required.' },
+    { id: 'message', errorMsgId: 'message-error', errMsg: 'Message is required.' }
+  ];
+
+  const removeError = (fieldId, errorMsgId) => {
+    const field = document.getElementById(fieldId);
+    field.addEventListener('input', function() {
+      if (this.value.trim()) {
+        document.getElementById(errorMsgId).style.display = 'none';
+        this.classList.remove('error-border');
+      }
+    });
+  };
+
+  fields.forEach(({ id, errorMsgId }) => removeError(id, errorMsgId));
   
+
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     
     let errors = 0;
     let errorMessages = [];
-    // USED WHEN TRIED TO BRING FOCUS TO FIRST ERROR FIELD
-    // let firstErrorField = null;
 
-    // REMOVES ERROR-BORDER CLASS FROM ALL FIELDS
     document.querySelectorAll('.error-border').forEach(el => el.classList.remove('error-border'));
 
-
-    // HIDES ALL PREVIOUS ERROR MESSAGES
     const errorMessagesEl = document.querySelectorAll('.error-message');
     errorMessagesEl.forEach(msg => msg.style.display = "none");
 
-    // VALIDATE FULL NAME
-    const fullNameField = document.getElementById('full-name');
-    const fullName = fullNameField.value.trim();
-    if (!fullName) {
-        document.getElementById('full-name-error').style.display = 'block';
-        fullNameField.classList.add('error-border');  // Add error-border class
-        errors++;
-        errorMessages.push('Full name is required.');
-    
-        // failed attemp to bring focus to first error field; brings focus to the field
-        // and reads the field info before reading the error messages; tried with both "polite" and "assertive
-        // firstErrorField = firstErrorField || 'full-name';
-    } 
+    fields.forEach(({ id, errorMsgId, errMsg }) => {
+      const fieldElement = document.getElementById(id);
+      const value = fieldElement.value.trim();
 
-    // VALIDATE EMAIL
-    const emailField = document.getElementById('email');
-    const email = emailField.value.trim();
-    if (!email) {
-        document.getElementById('missing-email').style.display = 'block';
-        emailField.classList.add('error-border');  
+      if (!value) {
+        document.getElementById(errorMsgId).style.display = 'block';
+        fieldElement.classList.add('error-border');
         errors++;
-        errorMessages.push('Email is required.');
-    }
-    // REPLACED BY THE BUILD-IN HTML5 EMAIL VALIDATION
-    // else if (!email.includes('@')) {
-    //     document.getElementById('missing-@').style.display = 'block';
-    //     errors++;
-    //     errorMessages.push("Email must include the at symbol.");
-    //     // firstErrorField = firstErrorField || 'email';
-    // }
+        errorMessages.push(errMsg);
+      }
+    });
 
-    // VALIDATE MESSAGE
-    const messageField = document.getElementById('message');
-    const message = messageField.value.trim();
-    if (!message) {
-        document.getElementById('message-error').style.display = 'block';
-        messageField.classList.add('error-border');  // Add error-border class
-        errors++;
-        errorMessages.push('Message is required.');
-  
-        // firstErrorField = firstErrorField || 'message';
-    }
-
-    const errorNoun = errors === 1 ? 'error' : 'errors';
-    const errorVerb = errors === 1 ? 'is' : 'are';
     if (errors > 0) {
+      const errorNoun = errors === 1 ? 'error' : 'errors';
+      const errorVerb = errors === 1 ? 'is' : 'are';
       formError.textContent = `There ${errorVerb} ${errors} ${errorNoun} in your submission. ${errorMessages.join(" ")}`;
-      // document.getElementById(firstErrorField).focus();
     }
   });
 });
